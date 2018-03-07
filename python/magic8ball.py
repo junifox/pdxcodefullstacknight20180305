87d41991de6e1cb78d3b8052890798b19b80475e
Juniper <juniperlacey@gmail.com>
2018-03-06 20:31:28 -0800

    magic 8ball lab

---
+++ python/magic8ball.py
@@ -0,0 +1,5 @@
+from random import choice
+print("Welcome")
+responses = ["It is certain", "It is decidedly so", "Without a doubt", "Definitely", "You may rely on it", "Yes", "Reply hazy try again", "No", "Don't count on it", "Very doubtful", "Outlook not so good", "lol nope"]
+question = input("What would you like to ask")
+print(choice(responses))
