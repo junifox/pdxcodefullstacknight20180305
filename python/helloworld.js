fe53956d054d24c990ba2ec72a6ec7ac4eb60b59
Juniper <juniperlacey@gmail.com>
2018-04-20 18:15:26 -0700

    hello world converted to js

---
+++ python/helloworld.js
@@ -0,0 +1,7 @@
+var description = " "
+var mySelf = { name: Juniper, job: Unemployed
+}
+var helloWorld = function(name, job) {
+  console.log("hello my name is" + name + " I am" + job);
+}
+    helloWorld(object.name)
