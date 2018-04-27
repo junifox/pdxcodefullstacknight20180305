2f3e1e397fdaa46cd08b86150221f398664f2c59
Juniper <juniperlacey@gmail.com>
2018-04-26 20:14:08 -0700

    random redirect prototype

--- javascript/helloworld.js
+++ javascript/helloworld.js
@@ -1,7 +1,6 @@
 var description = " "
-var mySelf = { name: Juniper, job: Unemployed
-}
+var mySelf = {name: "Juniper", job: "Unemployed"}
 var helloWorld = function(name, job) {
   console.log("hello my name is" + name + " I am" + job);
 }
-    helloWorld(object.name)
+    helloWorld(mySelf.name, mySelf.job)
